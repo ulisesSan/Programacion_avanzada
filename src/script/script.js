c141e755e3bf2a4144a13e57115b57284c8ddbc4
Ulises Santiago Caceres Licona <37051136+ulisesSan@users.noreply.github.com>
2021-02-23 18:12:36 -0600

    Pequeños cambios
    Se agrega js para mostrar la hora de manera continua

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,35 @@
-function fecha(){
-today = Date();
-document.log(today.getday());
-}
+const HORA = () => {
+
+    // Constante que almacena el id del elemento donde
+    // se va a mostrar el reloj
+    const ID_ELEMENT = document.getElementById("reloj");
+  
+    // Función que añade un cero a la izquierda
+    // a la hora, minutos y segundos cuando el
+    // valor de estos es inferior a 10
+    const CERO = n => n = n < 10 ? "0"+n: n;
+    let hora, minutos, segundos, meridiano;
+  
+    // Funcion que retorna el Reloj
+    const RELOJ = () => {
+        const DATE = new Date();
+        hora = DATE.getHours();
+        minutos = DATE.getMinutes();
+        segundos = DATE.getSeconds();
+  
+        // Determinar el meridiano
+        meridiano = hora < 12 ? "am" : "pm";
+  
+        // Dar formato de 12 horas ya que por defecto el formato es de 24 horas
+        hora = hora == 0 ? 12 : hora || hora > 12 ? hora -= 12 : hora;
+  
+        return (
+            ID_ELEMENT.textContent =
+            `${CERO(hora)}:${CERO(minutos)}:${CERO(segundos)} ${meridiano}`
+        );
+    }
+  
+    // Llama a la función RELOJ cada segundo
+    // para que se vaya actualizando la hora
+    return setInterval(RELOJ, 1000);
+  }
