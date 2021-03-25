25e1046a89db642dffdc5431f5be3a0dd80797b2
Ulises Santiago <uliseo_95@hotmail.com>
2021-03-25 09:30:19 -0600

    Tortas

--- 02_CLUS/F83_CLUS(individual)/Pactica 2/src/Codigo JS/codigo02.js
+++ 02_CLUS/F83_CLUS(individual)/Pactica 2/src/Codigo JS/codigo02.js
@@ -1,5 +1,25 @@
-function obtVar(){
-    var torta = document.getElementById("torta1");
+var obtVar = function (){
+    var huevo = document.getElementById("huevo").value;
+    var jamon = document.getElementById("jamon").value;
+    var milaneza = document.getElementById("milaneza").value;
+    var pollo = document.getElementById("pollo").value;
 
-    console.log(torta);
+    var conf = confirm("¿Está seguro de su pedido?");
+    if(conf ==true){
+        if(huevo == 0 && jamon == 0 && milaneza == 0 && pollo == 0){
+            alert("No está pidiendo ninguna torta");
+        }
+        else{
+        huevo = huevo * 45;
+        jamon = jamon *45;
+        milaneza = milaneza * 45;
+        pollo = pollo *45;
+        total= huevo+jamon+milaneza+pollo;
+        document.getElementById("precio").innerHTML = total;
+        }
+    }
+    
+    else{
+        alert("Operacion cancelada por el usuario");
+    }
 }
