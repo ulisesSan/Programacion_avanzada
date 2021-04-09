8306cfff5fb53c4c17bd5d47bde0520bd52a24ae
Ulises Caceres <uliseo_95@hotmail.com>
2021-04-08 23:46:05 -0500

    Reloj php

--- 02_CLUS/F83_CLUS(individual)/Practica 3/index.php
+++ 02_CLUS/F83_CLUS(individual)/Practica 3/index.php
@@ -1,3 +1,27 @@
 <?php
 
+$hora=idate("h");
+$minutos = idate("i");
+$segundos = idate("s");
+
+
+
+for($i=0;$i<=60;$i++){
+    if($minutos == 59){
+        $hora++;
+        $minutos==0;
+    }
+    if($segundos == 59){
+        $minutos ++;
+        $segundos==0;
+    }
+
+    $i++;
+    echo ($hora);
+    echo ($minutos);
+    echo (":");
+    echo ($segundos++);
+    sleep(3);
+    ncurses_clear ( );  
+}
 ?>
